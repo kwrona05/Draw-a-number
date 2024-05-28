702aac626677bba51c18616c6bc1c4b8cbc8032c
kwrona05 <kwrona05@gmail.com>
2024-05-28 06:03:23 +0200

    add if to my code

--- script.ts
+++ script.ts
@@ -1,7 +1,13 @@
 const button: HTMLButtonElement | null = document.querySelector('#randomizer');
 const paragraph: HTMLParagraphElement | null = document.querySelector('#result');
 
-button?.addEventListener('click', () => {
+if (button) {
+    button.addEventListener('click', () => {
     const result: number = Math.floor(Math.random() * 100);
-paragraph && paragraph.textContent = `Your drawn number is: ${result}`
+if (paragraph) {
+    paragraph.textContent = `Your drawn number is: ${result}`
+}
 })
+} else {
+    console.error('Button element not found')
+}
